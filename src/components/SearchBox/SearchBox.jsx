cac7132abdf6b153245e0e77aba85c36ed9761a9
DmytroNadolynskyi <113470234+DmytroNadolynskyi@users.noreply.github.com>
2023-01-25 23:21:37 +0200

    add search-component

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,28 @@
+import { NavBox,
+  SearchbarCss,
+  SearchForm,
+  SearchFormButton,
+  SearchFormButtonLabel,
+  SearchFormInput,
+} from './SearchBox.styled';
+  import { ReactComponent as Icon } from 'icons/zoom.svg';
+export const SearchBox = ({ searchRef, handleSubmit }) => {
+  return (
+    <NavBox>
+        <SearchbarCss>
+          <SearchForm onSubmit={handleSubmit}>
+            <SearchFormButton type="submit">
+              <Icon />
+              <SearchFormButtonLabel>Search</SearchFormButtonLabel>
+            </SearchFormButton>
+            <SearchFormInput
+              ref={searchRef}
+              type="text"
+              placeholder="Search movies"
+              required
+            />
+          </SearchForm>
+        </SearchbarCss>
+      </NavBox>
+  );
+};
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,18 +1,15 @@
-import { ReactComponent as Icon } from 'icons/zoom.svg';
+
+import { SearchBox } from 'components/SearchBox/SearchBox';
 
 import Loader from 'components/Loader/Loader';
 import React, { useEffect, useRef, useState } from 'react';
 import { useSearchParams, useLocation } from 'react-router-dom';
 import { getMovieByQuery } from 'services/API';
 import {
-  NavBox,
+  
   List,
   Link,
-  SearchbarCss,
-  SearchForm,
-  SearchFormButton,
-  SearchFormButtonLabel,
-  SearchFormInput,
+  
 } from './Movies.styled';
 
 export default function Movies() {
@@ -28,7 +25,7 @@ export default function Movies() {
   const handleSubmit = event => {
     event.preventDefault();
     setSearchParams({ query: searchRef.current.value });
-    searchRef.current.value = '';
+    // searchRef.current.value = '';
   };
 
   useEffect(() => {
@@ -45,22 +42,7 @@ export default function Movies() {
 
   return (
     <>
-      <NavBox>
-        <SearchbarCss>
-          <SearchForm onSubmit={handleSubmit}>
-            <SearchFormButton type="submit">
-              <Icon />
-              <SearchFormButtonLabel>Search</SearchFormButtonLabel>
-            </SearchFormButton>
-            <SearchFormInput
-              ref={searchRef}
-              type="text"
-              placeholder="Search movies"
-              required
-            />
-          </SearchForm>
-        </SearchbarCss>
-      </NavBox>
+      <SearchBox searchRef={searchRef} handleSubmit ={handleSubmit } />
       {isLoading ? (
         <Loader />
       ) : (
